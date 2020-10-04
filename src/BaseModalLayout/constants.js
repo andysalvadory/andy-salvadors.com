3278e53cb6cf71a57cc4a05cf9a59128e429d6af
andysalvadory <70211871+andysalvadory@users.noreply.github.com>
2020-10-04 19:10:47 +0300

    Initialize fix_NoBorderInput_import_mixin_bug

---
+++ src/BaseModalLayout/constants.js
@@ -0,0 +1,12 @@
+export const dataHooks = {
+  baseModalLayout: 'baseModalLayout',
+  closeButton: 'baseModalLayout-close-button',
+  headerTitle: 'baseModalLayout-title',
+  headerSubtitle: 'baseModalLayout-subtitle',
+  contentWrapper: 'baseModalLayout-content-wrapper',
+  footerDivider: 'baseModalLayout-footer-divider',
+  footerSideActions: 'baseModalLayout-side-actions',
+  footerPrimaryButton: 'baseModalLayout-primary-button',
+  footerSecondaryButton: 'baseModalLayout-secondary-button',
+  illustrationSrc: 'baseModalLayout-illustration-src',
+};
---
+++ src/Page/index.d.ts
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import PageHeader from '../PageHeader';
+
+export type PageProps = {
+  dataHook?: string;
+  backgroundImageUrl?: string;
+  maxWidth?: number;
+  minWidth?: number;
+  height?: string;
+  sidePadding?: number;
+  className?: string;
+  gradientClassName?: string;
+  scrollableContentRef?: (ref: HTMLElement) => void;
+  zIndex?: number;
+  horizontalScroll?: boolean;
+};
+
+export interface ContentProps {
+  children: React.ReactNode;
+  fullScreen?: boolean;
+}
+declare const Content: React.SFC<ContentProps>;
+
+export interface FixedContentProps {
+  children: React.ReactElement;
+}
+declare const FixedContent: React.SFC<FixedContentProps>;
+
+export interface TailProps {
+  children: React.ReactElement;
+  minimized?: boolean;
+}
+declare const Tail: React.SFC<TailProps>;
+
+export interface StickyProps {
+  children: React.ReactElement | StickyChildrenRenderFn;
+  style?: React.CSSProperties;
+}
+type StickyChildrenRenderFn = (data: {
+  style: string;
+  className: string;
+}) => React.ReactElement;
+declare const Sticky: React.SFC<StickyProps>;
+
+export default class Page extends React.PureComponent<PageProps> {
+  static Header: typeof PageHeader;
+  static Content: typeof Content;
+  static FixedContent: typeof FixedContent;
+  static Tail: typeof Tail;
+  static Sticky: typeof Sticky;
+}
